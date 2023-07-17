9d17f3fdf4b80b4bb1472fd10469143d8accdad6
Peyman Khalili <iampeymankhalili@gmail.com>
2023-07-17 17:24:01 +0330

    dbsignup.php file to perform registration operations and contains commands to connect to the database.

---
+++ database/dbsignup.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    require_once "dbconnection.php";
+    require_once "functions.php";
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $re_password = $_POST["re_password"];
+
+    if(emptyInputSignup($name, $email, $password, $re_password) !== false){
+        header("location: ../signup.php?error=emptyinput");
+    }
+
+    if(invalidEmail($email)){
+        header("location: ../signup.php?error=invalidEmail");
+    }
+
+    if(passwordMatch($password, $re_password)){
+        header("location: ../signup.php?error=passworddonotmatch");
+    }
+
+    createUser($conn, $name, $email, $password);
+
+
+} else {
+    header("location: ../signup.php");
+}
+
+
+?>

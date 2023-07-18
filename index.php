9374c6cc34eb6b3dd21c15975b47b285b1e9653f
Peyman Khalili <iampeymankhalili@gmail.com>
2023-07-18 17:31:18 +0330

    Add the codes related to displaying the error message of the error functions.

--- index.php
+++ index.php
@@ -92,24 +92,6 @@ session_start();
                 </nav>
             </div>
         </div>
-        <!-- row -->
-        <div class="row tm-content-row tm-mt-big">
-            <div class="tm-col tm-col-big">
-                <div class="bg-white tm-block h-100">
-                    <h2 class="tm-block-title">Performance</h2>
-                    <div class="row">
-                        <div class="col-12 col-sm-4">
-                            <button type="submit" class="btn btn-primary">Update
-                            </button>
-                        </div>
-                        <div class="col-12 col-sm-8 tm-btn-right">
-                            <button type="submit" class="btn btn-danger">Delete Account
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 <script src="js/jquery-3.3.1.min.js"></script>

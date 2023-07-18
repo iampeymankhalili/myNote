60d203bb761be722516e38bde8853f92cdf35dfe
Peyman Khalili <iampeymankhalili@gmail.com>
2023-07-18 17:38:09 +0330

    Add error functions related to login that are called.

--- database/dblogin.php
+++ database/dblogin.php
@@ -10,6 +10,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
+    if(emptyInputLogin($email, $password)){
+        header("location: ../signup.php?error=emptyinput");
+    }
+
+    if(invalidEmail($email)){
+        header("location: ../signup.php?error=invalidEmail");
+    }
+
     loginUser($conn, $email, $password);
 
 
